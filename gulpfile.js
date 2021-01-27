a83b0120c0a3c8d8a3da77f6a1c6f387edffeba0
Mack Raicevic <makiraicevic@gmail.com>
2021-01-27 00:26:52 -0800

    adding all code to my portfolio repo

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+const dir = {
+      build: './src/build/',
+      theme: './src/theme/'
+};
+
+let gulp          = require('gulp'),
+    concat        = require('gulp-concat'),
+    sass          = require('gulp-sass'),
+    autoprefixer  = require('gulp-autoprefixer'),
+    sourcemaps    = require('gulp-sourcemaps'),
+    addsrc        = require('gulp-add-src'),
+    rename        = require('gulp-rename'),
+    browserSync   = require('browser-sync').create();
+
+
+
+// Browser Sync task
+gulp.task('browser-sync', function() {
+  let files = [
+    dir.theme + '*.html',
+    dir.build + 'css/**/*.css',
+    dir.build + 'sass/**/*.scss',
+    dir.build + 'js/**/*.js',
+    dir.theme + 'js/**/*.js'
+  ];
+  // Initialize BrowserSync
+  browserSync.init(files,{
+    // server: './src/theme'
+    server: {
+      baseDir: './src/theme'
+      // index: "index.php"
+    }
+    
+  });
+
+});
+
+let autoprefixerOptions = { browsers: ['last 2 versions', '> 5%', 'Firefox ESR'] };
+
+gulp.task('sass', function(){
+  return gulp.src(dir.build + 'sass/main.scss')
+  .pipe(sourcemaps.init( {loadMaps: false} ))
+  .pipe(sass({ errLogToConsole: true }).on('error', sass.logError))
+  .pipe(autoprefixer(autoprefixerOptions))
+  .pipe(rename('style.css'))
+  .pipe(sourcemaps.write())
+
+  // .pipe(concat('style.css'))
+  .pipe(gulp.dest(dir.theme +'.'))
+  .pipe(browserSync.stream())
+});
+// exports.sass = sass;
+
+
+// Watch Task
+gulp.task('default', ['browser-sync', 'sass'], function() {
+  gulp.watch(dir.build + 'sass/*.scss', ['sass']);
+  gulp.watch(dir.theme + '*.html');
+});
+
+
